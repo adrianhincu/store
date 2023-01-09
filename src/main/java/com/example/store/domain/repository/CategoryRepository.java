b2e1831d43544b00799904ef23b889f18ace7a4e
adrian <hincu.adrian1@gmail.com>
2023-01-09 20:01:41 +0200

    Added CategoryRepository.java

---
+++ src/main/java/com/example/store/domain/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.store.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+}
