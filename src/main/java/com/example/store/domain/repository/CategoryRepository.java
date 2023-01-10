7e122b1e386fc8e41d374f86de085dad9965ebcb
adrian <hincu.adrian1@gmail.com>
2023-01-10 16:05:10 +0200

    fix CategoryRepository.java.java

--- src/main/java/com/example/store/domain/repository/CategoryRepository.java
+++ src/main/java/com/example/store/domain/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.store.domain.repository;
 
+import com.example.store.domain.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
